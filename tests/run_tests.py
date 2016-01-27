8e09c44b0e43ff19421fa6e2fb97a6070ba0e509
Jacopo Canton <jcanton@mech.kth.se>
2016-01-27 15:47:15 +0100

    added first version of run_tests.py

---
+++ tests/run_tests.py
@@ -0,0 +1,22 @@
+#------------------------------------------------------------------------------
+# test nek scripts
+#
+def test_nek_scripts():
+	import sys
+	sys.path.append('../src/')
+	import neksuite as ns
+	import numpy as np
+	import scipy.interpolate as spi
+	#import matplotlib.pyplot as plt
+	#from mayavi import mlab
+	import time
+
+	fname = 'nek/channel3D_0.f00001'
+
+	return 0
+
+#------------------------------------------------------------------------------
+# test simson scripts
+#
+def test_simson_scripts():
+	return 1

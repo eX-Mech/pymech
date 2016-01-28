772b367a515ecad9e9e32ffbfd07c78f6b7cd15c
Jacopo Canton <jcanton@mech.kth.se>
2016-01-28 16:50:23 +0100

    unified neksuite and simsonsuite with exadata

--- src/simsonsuite.py
+++ src/simsonsuite.py
@@ -9,62 +9,7 @@
 #=============================================================================#
 import struct
 import numpy as np
-
-
-#==============================================================================
-class datalims:
-	"""
-	    datalims
-	    A class containing the extrema of all quantities stored in the mesh
-	"""
-	def __init__(self, var):
-		#                    x,y,z   min,max
-		self.pos  = np.zeros((3     , 2))
-		#                    u,v,w   min,max
-		self.vel  = np.zeros((3     , 2))
-		#                    p       min,max
-		self.pres = np.zeros((var[2], 2))
-		#                    T       min,max
-		self.temp = np.zeros((var[3], 2))
-		#                    s_i     min,max
-		self.scal = np.zeros((var[4], 2))
-
-#==============================================================================
-class elem:
-	"""
-	    elem
-	    A class containing one nek element/SIMSON flow field
-	"""
-	def __init__(self, var, lr1):
-		#                    x,y,z   lz      ly      lx
-		self.pos  = np.zeros((3     , lr1[2], lr1[1], lr1[0]))
-		#                    u,v,w   lz      ly      lx
-		self.vel  = np.zeros((3     , lr1[2], lr1[1], lr1[0]))
-      #                    p       lz      ly      lx     
-		self.pres = np.zeros((var[2], lr1[2], lr1[1], lr1[0]))
-      #                    T       lz      ly      lx     
-		self.temp = np.zeros((var[3], lr1[2], lr1[1], lr1[0]))
-      #                    s_i     lz      ly      lx     
-		self.scal = np.zeros((var[4], lr1[2], lr1[1], lr1[0]))
-
-#==============================================================================
-class exadata:
-	"""
-	    data
-	    A class containing data for reading/writing binary simulation files
-	"""
-	def __init__(self, ndim, nel, lr1, var):
-		self.ndim   = ndim
-		self.nel    = nel
-		self.var    = var
-		self.lr1    = lr1
-		self.time   = []
-		self.istep  = []
-		self.wdsz   = []
-		self.endian = []
-		self.lims   = datalims(var)
-		self.elem   = [elem(var, lr1) for i in range(nel)]
-
+import exadata as exdat
 
 #==============================================================================
 def readdns(fname):
@@ -223,7 +168,7 @@ def readdns(fname):
 	      1j*np.zeros((lr1[2],lr1[1],lr1[0]/2+1))
 	#
 	# initialize data structure
-	data = exadata(ndim, 1, lr1, var)
+	data = exdat.exadata(ndim, 1, lr1, var)
 	data.time   = time
 	data.wdsz   = wdsz
 	if (emode == '<'):

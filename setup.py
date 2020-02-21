1bc0aa72e384d137ee341e3ecfa369357a49f12e
Jacopo Canton <jacopo.canton@gmail.com>
2020-02-21 10:09:19 +0100

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='pymech',
-    version='1.1',
+    version='1.1.1',
     author='Jacopo Canton & Nicolo Fabbiane',
     author_email='jacopo.canton@gmail.com',
     description='A Python suite of routines for Nek5000 and Simson.',

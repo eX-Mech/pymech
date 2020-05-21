47191f8d4634709ca2b5fdf2f49694ef3c0bf26c
Ashwin Vishnu <9155111+ashwinvis@users.noreply.github.com>
2020-05-21 23:06:16 +0200

    Function open_nek_dataset and class _NekDataStore
    The store is a more low-level representation which can be converted into an
    xarray dataset. Peformance gain: 3.45 to 3.1 seconds.
    
    Changed: xmesh, ymesh, zmesh are variables and not coordinates.

--- pymech/dataset.py
+++ pymech/dataset.py
@@ -10,19 +10,44 @@ def open_dataset(path):
 	"""Helper function for opening a file as an xarray dataset."""
 	path = Path(path)
 	if path.suffix.startswith('.f'):
-		cls = NekDataset
+		_open = open_nek_dataset
 	else:
 		raise NotImplementedError(
 			"Filetype: {} is not supported.".format(path.suffix)
 		)
 
-	return cls(path).open()
+	return _open(path)
 
 
-class NekDataset:
+#  @profile
+def open_nek_dataset(path):
 	"""Interface for converting Nek field files into xarray datasets"""
-	def __init__(self, path):
-		self.field = readnek(path)
+	field = readnek(path)
+	if isinstance(field, int):
+		raise IOError("Failed to load {}".format(path))
+
+	elements = field.elem
+	elem_stores = [_NekDataStore(elem) for elem in elements]
+	elem_dsets = [
+		xr.Dataset.load_store(store).set_coords(store.axes)
+		for store in elem_stores
+	]
+
+	# See: https://github.com/MITgcm/xmitgcm/pull/200
+	if xr.__version__ < '0.15.2':
+		ds = xr.combine_by_coords(elem_dsets)
+	else:
+		ds = xr.combine_by_coords(elem_dsets, combine_attrs="drop")
+
+	ds.coords.update({"time": field.time})
+
+	return ds
+
+
+class _NekDataStore(xr.backends.common.AbstractDataStore):
+	"""Xarray store for a Nek field element."""
+	def __init__(self, elem):
+		self.elem = elem
 		self.axes = ("z", "y", "x")
 
 	def meshgrid_to_dim(self, mesh):
@@ -33,11 +58,31 @@ class NekDataset:
 		dim = np.unique(np.round(mesh, 8))
 		return dim
 
-	def elem_to_dataset(self, elem):
+	def get_dimensions(self):
+		return self.axes
+
+	def get_attrs(self):
+		elem = self.elem
+		attrs = {
+			"boundary_conditions": elem.bcs,
+			"curvature": elem.curv,
+			"curvature_type": elem.ccurv,
+		}
+		return attrs
+
+	#  @profile
+	def get_variables(self):
 		"""Generate an xarray dataset from a single element."""
 		ax = self.axes
+		elem = self.elem
 
 		data_vars = {
+			ax[2]: self.meshgrid_to_dim(elem.pos[0]),  # x
+			ax[1]: self.meshgrid_to_dim(elem.pos[1]),  # y
+			ax[0]: self.meshgrid_to_dim(elem.pos[2]),  # z
+			"xmesh": (ax, elem.pos[0]),
+			"ymesh": (ax, elem.pos[1]),
+			"zmesh": (ax, elem.pos[2]),
 			"ux": (ax, elem.vel[0]),
 			"uy": (ax, elem.vel[1]),
 			"uz": (ax, elem.vel[2]),
@@ -56,28 +101,4 @@ class NekDataset:
 				}
 			)
 
-		ds = xr.Dataset(
-			data_vars=data_vars,
-			coords={
-				ax[2]: self.meshgrid_to_dim(elem.pos[0]),  # x
-				ax[1]: self.meshgrid_to_dim(elem.pos[1]),  # y
-				ax[0]: self.meshgrid_to_dim(elem.pos[2]),  # z
-				"xmesh": (ax, elem.pos[0]),
-				"ymesh": (ax, elem.pos[1]),
-				"zmesh": (ax, elem.pos[2]),
-			},
-			attrs={
-				"boundary_conditions": elem.bcs,
-				"curvature": elem.curv,
-				"curvature_type": elem.ccurv,
-			}
-		)
-
-
-		return ds
-
-	def open(self):
-		elements = self.field.elem
-		ds = xr.combine_by_coords(map(self.elem_to_dataset, elements))
-		ds.coords.update({"time": self.field.time})
-		return ds
+		return data_vars

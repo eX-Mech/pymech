f55c88afba49193f32edbc2c67852af59e0c798e
Ashwin Vishnu <9155111+ashwinvis@users.noreply.github.com>
2020-07-08 22:27:04 +0200

    Separate requirements in setup.cfg
    Docs and tests requirements mentioned as extras

--- setup.py
+++ setup.py
@@ -1,22 +1,4 @@
 import setuptools
 
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
 
-setuptools.setup(
-    name='pymech',
-    version='1.2.0',
-    author='Jacopo Canton, Nicolo Fabbiane and Guillaume Chauvat',
-    author_email='jacopo.canton@gmail.com',
-    description='A Python suite of routines for Nek5000 and Simson.',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url='https://github.com/jcanton/pymech',
-    packages=['pymech'],
-    install_requires=['xarray>=0.15'],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Operating System :: OS Independent",
-    ],
-)
+setuptools.setup()

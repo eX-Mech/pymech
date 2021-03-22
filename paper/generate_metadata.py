ee9f63321c42e5d66a405fb2a8b0d77376d95a5a
Ashwin Vishnu <y4d71nsar@relay.firefox.com>
2021-03-22 23:03:13 +0100

    Blacken generate_metadata script

--- paper/generate_metadata.py
+++ paper/generate_metadata.py
@@ -11,47 +11,47 @@ import ruamel.yaml
 here = Path.cwd()
 
 with (here.parent / "codemeta.json").open() as fp:
-	cm = load(fp)
+    cm = load(fp)
 
 
 #  print(cm)
 
 
 def ordered_set(iterable):
-	return OrderedDict.fromkeys(iterable).keys()
+    return OrderedDict.fromkeys(iterable).keys()
 
 
 uniq_affiliations = list(ordered_set(author["affiliation"] for author in cm["author"]))
 
 
 def make_author(author):
-	data = {
-		"name": " ".join([author["givenName"], author["familyName"]]),
-		"affiliation": uniq_affiliations.index(author.get("affiliation", "")) + 1,
-	}
-	orcid_url = author.get("@id")
-	if orcid_url:
-		data.update({"orcid": Path(orcid_url).name})
-	return data
+    data = {
+        "name": " ".join([author["givenName"], author["familyName"]]),
+        "affiliation": uniq_affiliations.index(author.get("affiliation", "")) + 1,
+    }
+    orcid_url = author.get("@id")
+    if orcid_url:
+        data.update({"orcid": Path(orcid_url).name})
+    return data
 
 
 def make_affiliations():
-	return [{"name": name, "index": i+1} for i, name in enumerate(uniq_affiliations)]
+    return [{"name": name, "index": i + 1} for i, name in enumerate(uniq_affiliations)]
 
 
 def make_date(date):
-	date_parsed = datetime.strptime(date, "%Y-%m-%d")
-	return datetime.strftime(date_parsed, "%d %B %Y")
+    date_parsed = datetime.strptime(date, "%Y-%m-%d")
+    return datetime.strftime(date_parsed, "%d %B %Y")
 
 
 # Prepare data for metadata output
 data = {
-	"title": cm.get("title", ""),
-	"tags": cm.get("keywords", []),
-	"authors": [make_author(_) for _ in cm.get("author", [{}])],
-	"affiliations": make_affiliations(),
-	"date": make_date(cm.get("dateModified", "")),
-	"bibliography": "paper.bib",
+    "title": cm.get("title", ""),
+    "tags": cm.get("keywords", []),
+    "authors": [make_author(_) for _ in cm.get("author", [{}])],
+    "affiliations": make_affiliations(),
+    "date": make_date(cm.get("dateModified", "")),
+    "bibliography": "paper.bib",
 }
 
 
@@ -61,14 +61,14 @@ data = {
 # yaml.allow_unicode = True
 # yaml.indent(sequence=4, offset=2, mapping=4)
 def dump(data, dest):
-	# yaml.dump(data, dest)
-	ruamel.yaml.dump(
-		data,
-		dest,
-		explicit_start=True,
-		allow_unicode=True,
-		Dumper=ruamel.yaml.RoundTripDumper
-	)
+    # yaml.dump(data, dest)
+    ruamel.yaml.dump(
+        data,
+        dest,
+        explicit_start=True,
+        allow_unicode=True,
+        Dumper=ruamel.yaml.RoundTripDumper,
+    )
 
 
 dest = sys.stdout

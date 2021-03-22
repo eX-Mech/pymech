b77dfc6a5da70c80575919aba69725a35d07dbdd
Ashwin Vishnu <y4d71nsar@relay.firefox.com>
2021-03-22 16:18:31 +0100

    Replace colorlog with rich

--- pymech/log.py
+++ pymech/log.py
@@ -7,21 +7,10 @@ logger = logging.getLogger("pymech")
 # Initialize logging
 try:
     # Set a nice colored output
-    from colorlog import ColoredFormatter
+    from rich.logging import RichHandler
 
-    formatter = ColoredFormatter(
-        "%(log_color)s%(levelname)-8s%(reset)s %(blue)s%(message)s",
-        log_colors={
-            "DEBUG": "cyan",
-            "INFO": "green",
-            "WARNING": "yellow",
-            "ERROR": "red",
-            "CRITICAL": "red",
-        },
-    )
-    stream = logging.StreamHandler()
-    stream.setFormatter(formatter)
-    logger.addHandler(stream)
+    handler = RichHandler()
+    logger.addHandler(handler)
 except ImportError:
     # No color available, use default config
     logging.basicConfig(format="%(levelname)s: %(message)s")

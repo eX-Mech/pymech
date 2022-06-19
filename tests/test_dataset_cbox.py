bb0829dd342460efc5ae3a0d99968108e78038ce
Ashwin V. Mohanan <dev@fluid.quest>
2022-06-19 09:31:43 +0200

    Tests to reproduce issues
    See #48 #56

---
+++ tests/test_dataset_cbox.py
@@ -0,0 +1,9 @@
+import pytest
+import pymech as pm
+
+
+@pytest.mark.parametrize(
+    "file", ["cbox0.fld", "cbox0_issue_48.fld", "cbox0_issue_56.fld"]
+)
+def test_cbox(file, test_data_dir):
+    pm.open_dataset(test_data_dir / "nek" / file)

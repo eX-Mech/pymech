5f36fac7c54ea6df8f78d586473f1c2d9ab63140
Ashwin V. Mohanan <dev@fluid.quest>
2022-10-23 20:26:16 +0200

    Run asv benchmark with skip_vars

--- docs/asv_bench/benchmarks/bench_readnek.py
+++ docs/asv_bench/benchmarks/bench_readnek.py
@@ -5,3 +5,9 @@ from . import Channel3D
 class ReadnekSuite(Channel3D):
     def time_read(self):
         readnek(self.file)
+
+    def time_read_skip_geom(self):
+        readnek(self.file, skip_vars=("x", "y", "z"))
+
+    def time_read_skip_ux_uy(self):
+        readnek(self.file, skip_vars=("ux", "uy"))

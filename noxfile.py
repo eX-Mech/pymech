15f4a739dc8ea8b95d3a1fc41d876a6c8579392d
Ashwin V. Mohanan <dev@fluid.quest>
2024-03-03 02:56:09 +0100

    Limit mayavi to Python 3.9 and 3.10

--- noxfile.py
+++ noxfile.py
@@ -50,7 +50,7 @@ def tests(session):
     )
 
 
-@session(name="tests-cov-vtk")
+@session(name="tests-cov-vtk", python=["3.9", "3.10"])
 def tests_cov_vtk(session):
     """Execute unit-tests using pytest+pytest-cov+VTK dependencies"""
     session.install(".[tests,vtk]")
